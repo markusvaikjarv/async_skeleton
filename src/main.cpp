8c6ec74144b224c5d7056fec4f47d26856273616
mavaik <mavaik@ttu.ee>
2020-06-03 21:52:58 +0300

    Added external event loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
+#include <future>
+#include <memory>
+#include <asio.hpp>
+
 #include "../include/Simple-Web-Server/client_http.hpp"
 #include "../include/Simple-Web-Server/server_http.hpp"
-#include <future>
 
 using namespace std;
 
@@ -8,17 +11,24 @@ using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
 
 int main() {
 	HttpServer server;
+
+	// Externally created io_context can be shared by other
+	// ASIO compatible libraries - e.g. async database drivers
+	shared_ptr<asio::io_context> loop = make_shared<asio::io_context>();
+
 	server.config.port = 8080;
+	server.io_service = loop;
 
 	server.resource["^/match/([0-9]+)$"]["GET"] = [](shared_ptr<HttpServer::Response> res, shared_ptr<HttpServer::Request> req) {
 		res->write(req->path_match[1].str());
 	};
 
 	promise<unsigned short> server_port;
-	thread server_thread([&server, &server_port]() {
+	thread server_thread([&server, &server_port, &loop]() {
 		server.start([&server_port](unsigned short port) {
 			server_port.set_value(port);
 		});
+		loop->run();
 	});
 	cout << "Server listening on PORT " << server_port.get_future().get() << endl << endl;
 
